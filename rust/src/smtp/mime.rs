86d284344e2adab7de3e7d70ee98081a109a7912
Shivani Bhardwaj <shivanib134@gmail.com>
2020-12-17 13:07:32 +0530

    mime hit and trial

--- rust/src/smtp/mime.rs
+++ rust/src/smtp/mime.rs
@@ -1,10 +1,12 @@
+extern crate mime;
+
 #[cfg(test)]
 mod test {
-    use mime::Mime;
-
     #[test]
     fn test_mime_dec_parse_line_01() {
-        let mime = Mime::from_str("From: Sender1");
-        assert_eq!(mime.source, 1);
+        // common types are constants
+        let text = mime::TEXT_PLAIN;
+
+        assert_eq!(text.subtype(), mime::PLAIN);
     }
 }
